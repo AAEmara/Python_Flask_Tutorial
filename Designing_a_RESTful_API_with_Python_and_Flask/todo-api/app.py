cad7eaf5fce89aeb2988cec87fb166c1491870a6
AAEmara <abdelrahmanemara18@gmail.com>
2024-03-30 16:16:30 +0200

    DesignREST: Modified app.py to GET tasks by `id`
    Added another function to allow the Client to
    retrieve the desired tasks resource by `id`
    given in the URI.

--- Designing_a_RESTful_API_with_Python_and_Flask/todo-api/app.py
+++ Designing_a_RESTful_API_with_Python_and_Flask/todo-api/app.py
@@ -2,15 +2,44 @@
 """Creating a simple web application."""
 
 
-from flask import Flask
+from flask import Flask, jsonify, abort
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    "Routing to the root page."""
-    return "Hello, World!"
+
+tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Need to find a good python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    """Routing to todo web service to retrieve the `tasks` resource."""
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    """Retrieving the `tasks` resource given by a specific task_id value."""
+    if len(tasks):
+        for task in tasks:
+            if task['id'] == task_id:
+                task_retrieved = task
+                return jsonify({'task': task_retrieved})
+
+    abort(404)
 
 
 if __name__ == "__main__":
